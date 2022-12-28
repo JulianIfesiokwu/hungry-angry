9fdb5715a0500c2e7010abd5cbcfd1540fe44fa6
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2022-12-28 23:21:30 +0100

    adding gallery route and gallery component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage/HomePage.Page';
 import AboutPage from './pages/AboutPage/AboutPage.Page';
 import ContactUs from './pages/ContactUsPage/ContactUs.Page';
 import ErrorPage from './pages/ErrorPage/ErrorPage.Page';
+import Gallery from './pages/GalleryPage/Gallery.page';
 import './App.css';
 import Volunteer from './pages/VolunteerPage/Volunteer.Page';
 import Donate from './pages/DonatePage/Donate.Page';
@@ -20,6 +21,7 @@ function App() {
           <Route path="/volunteer" element={<Volunteer />} />
           <Route path="/donate" element={<Donate />} />
           <Route path="/train" element={<Train />} />
+          <Route path="/gallery" element={<Gallery />} />
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
