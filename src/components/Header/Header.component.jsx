0e5ed3954e224f275b0e21e7083130bab27135ae
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2022-12-30 18:15:42 +0100

    removed unused code-stickyNav- and fixed navlinks to disappear after click on mobile

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,43 +1,36 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import { MdMenu, MdClose } from "react-icons/md";
 import Logo from "../../assets/logo.png";
 import './Header.styles.css';
 
 const HeaderAndNavigation = () => {
   const [menu, showMenu] = useState(false);
-  const [pageOffset, setPageOffset] = useState(false);
 
   const toggleMenu = () => {
     showMenu(!menu);
   };
 
-  const stickyNav = () => {
-    if(window.pageYOffset > 300) {
-      setPageOffset(true)
-    } else {
-      setPageOffset(false)
-    };
-  };
-
   return (
-    <header className={pageOffset ? 'header dark' : 'header'}>
+    <header className='header dark'>
         <div className="row-two fixed-to-top">
-            <div className="search-section">
-              <div className="logo-container">
-              <img src={Logo} alt="logo" className='logo'/>
-            </div>
+            <div className="search-section">              
+              <Link to='/'>
+                <div className="logo-container">
+                <img src={Logo} alt="logo" className='logo'/>
+                </div>
+              </Link>
             <span className='company-name'>Hungry & Angry</span>
-            {menu ? <MdMenu size="35" onClick={toggleMenu} color="#FFF" className='menu-icon'/> : <MdClose size="35" onClick={toggleMenu} color="#FFF" className='menu-icon'/>}
+            {menu ? <MdClose size="35" onClick={toggleMenu} color="#FFF" className='menu-icon'/> : <MdMenu size="35" onClick={toggleMenu} color="#FFF" className='menu-icon'/>}
             </div>
-            <div className={menu ? "nav-links" : "nav-links show"}>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/contact-us">Contact</NavLink>
-                <NavLink to="/gallery">Gallery</NavLink>
-                <NavLink to="/volunteer">Volunteer</NavLink>
-                <NavLink to="/train">Train</NavLink>
-                <NavLink to="/donations">Donations</NavLink>
+            <div className={menu ? "nav-links show" : "nav-links"}>
+                <NavLink to="/" onClick={() => showMenu(!menu)}>Home</NavLink>
+                <NavLink to="/about" onClick={() => showMenu(!menu)}>About</NavLink>
+                <NavLink to="/contact-us" onClick={() => showMenu(!menu)}>Contact</NavLink>
+                <NavLink to="/gallery" onClick={() => showMenu(!menu)}>Gallery</NavLink>
+                <NavLink to="/volunteer" onClick={() => showMenu(!menu)}>Volunteer</NavLink>
+                <NavLink to="/train" onClick={() => showMenu(!menu)}>Train</NavLink>
+                <NavLink to="/donate" onClick={() => showMenu(!menu)}>Donations</NavLink>
             </div>
         </div>
     </header>
