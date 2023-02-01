a3a47dac6dd98294267f01e2a8f6b0d0266de316
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2023-02-01 12:49:44 +0100

    feat: changed card structure removed title and summary props and made card a link to different pages

--- src/components/Card/Card.component.jsx
+++ src/components/Card/Card.component.jsx
@@ -2,20 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Card.styles.css';
 
-const Card = ({img,title,summary,btnTitle,link}) => {
+const Card = ({ img, btnTitle, link }) => {
   return (
-    <div className='card'>
-      <div className="front">
-        <div className="card-image-container">
-            <img src={img} alt="" />
-        </div>
-        <div className="card-details">
-            <h3 className='card-title'>{title}</h3>
-            <p className="summary">{summary}</p>
-            <Link className='card-btn' type='button' to={link}>{btnTitle}</Link>
-        </div>
+    <Link to={link} className='card'>
+      <img src={img} alt="" />
+      <div className="content">
+        <h3 className='card-title'>{btnTitle}</h3>
       </div>
-    </div>
+    </Link>
   )
 };
 
