8cfb4ce567ff9a2b9b6fac1feb07808d3f751099
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2023-02-01 13:01:47 +0100

    feat: changed card props, changed landing image fixed link in landing section

--- src/pages/HomePage/HomePage.Page.jsx
+++ src/pages/HomePage/HomePage.Page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import carouselImage1 from "../../assets/carousel (1).jpeg";
-import cardImage1 from "../../assets/cardImage (1).jpeg";
-import cardImage2 from "../../assets/cardImage (2).jpeg";
+import carouselImage1 from "../../assets/homepage_landing.jpg";
 import cardImage3 from "../../assets/cardImage (3).jpeg";
+import volunteerImage from "../../assets/volunteer.jpg";
+import donateImage from "../../assets/donate.jpg";
+import trainImage from "../../assets/train.jpg";
 import conclusionImage1 from "../../assets/conclusionImage (1).jpeg";
-import conclusionImage2 from "../../assets/conclusionImage (2).jpeg";
 import Card from '../../components/Card/Card.component';
 import "./HomePage.Page.styles.css";
 
@@ -18,27 +18,35 @@ const HomePage = () => {
             <img src={carouselImage1} alt="" className='landing-image' />
           </div>
           <div className="landing-text">
-            <h1 className='landing-major'>A wealth <span className='highlight'>creation</span> & <span className='highlight'>generation</span> NGO.</h1>
-            <Link to="/donations" className='donations-cta'>Donate to a Cause</Link>
+            <h1 className='landing-major'>Give a <span className='highlight'>helping hand</span> to those <span className='highlight'>who need it.</span></h1>
+            <Link to="/donate" className='donations-cta'>Donate to a Cause</Link>
           </div>
       </div>
       <section className="welcome">
-        <div className="column-two">
-          <h2 className="welcome-title">What We Are Doing</h2>
-          <p className='welcome-info-one'>We are on a mission to help the helpless.</p>
-          <p className="welcome-info">We are positioning ourselves to become a wealth generating network, where ordinary self driven individuals can converge and innovate, push boundaries and open doors. Hungry and Angry is attempting to bring people with diverse professions to come together under one umbrella for exchange learning.
+        <h2 className="welcome-header">About Us</h2>
+        <div className="welcome-content">
+          <div className="column-two">
+          <h3 className="welcome-title">Who We Are</h3>
+          <p className="welcome-info">Based in Anambra state, Nigeria, Hungry & Angry is a not for profit Non Government Organisation with the aim to empower, educate and invest in local communities to create and facilitate their development.
+          </p>
+          <h3 className="welcome-title">Our Aim</h3>
+          <p className='welcome-info-one'>We are on a mission to help the less priviledged.</p>
+          <p className="welcome-info">We seek to impact local communities with individual grants to persons seeking to develop small businesses, provide training for persons with disabilities, invest in local youths by offering business workshops, training and mentoring.
           </p>
           <Link className='about-cta' to='/about'>Learn more...</Link>
         </div>
         <div className="welcome-image-container">
           <img src={cardImage3} alt='' />
         </div>
+        </div>
       </section>
       <section className="more-info">
-        <Card img={cardImage1} title={`Volunteering`} summary={`It became necessary to seek new ways to complement the efforts of government`} btnTitle={`Volunteer with Us`} link={`/volunteer`}/>
-        <Card img={cardImage2} title={`Trainers`} summary={`Hungry and Angry attempts to bring together people with diverse professions under ...`} btnTitle={`Train with Us`} link={`/train`}/>
-        <Card img={cardImage3} title={`Donations`} summary={`Hungry and Angry attempts to bring together people with diverse professions under ...`} btnTitle={`Donate to a Cause`} link={`/donate`}/>
-        <Card img={cardImage3} title={`Donations`} summary={`Hungry and Angry attempts to bring together people with diverse professions under ...`} btnTitle={`Donate to a Cause`} link={`/donate`}/>
+        <h2 className="more-info-header">What We Do</h2>
+        <div className="more-info-content">
+          <Card img={volunteerImage} btnTitle={`Volunteer`} link={`/volunteer`}/>
+          <Card img={trainImage} btnTitle={`Train`} link={`/train`}/>
+          <Card img={donateImage} btnTitle={`Donate`} link={`/donate`}/>
+        </div>
         </section>
       <section className='conclusion'>
         <article className='article'>
@@ -50,7 +58,7 @@ const HomePage = () => {
         <article className='article'>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus ullam fugit sed repudiandae illo? Beatae cumque alias, quisquam facere optio blanditiis laudantium, libero deleniti esse quam, ducimus accusantium?</p>
           <div className="article-img-container">
-            <img src={conclusionImage2} alt="" />
+            <img src={conclusionImage1} alt="" />
           </div>
         </article>
       </section>
